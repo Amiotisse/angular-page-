1f538a94cf3b460a901d83abf1c242039361a334
Amina <himri.amina@gmail.com>
2017-03-24 15:19:50 +0100

    implementer le service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserRoleService} from "../user-role.service";
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userRoleService: UserRoleService ) { }
 
   ngOnInit() {
   }
--- src/app/user-role.service.ts
+++ src/app/user-role.service.ts
@@ -4,5 +4,15 @@ import { Injectable } from '@angular/core';
 export class UserRoleService {
 
   constructor() { }
-
+  user_roles = [
+    {
+      display : "Enseignant",
+      value :"teacher",
+    },
+    {
+      display : "Etudiant",
+      value : "student",
+    }
+  ];
+  user_roles_selected = this.user_roles[0];
 }
