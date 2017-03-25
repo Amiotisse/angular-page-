a293caad43afaf564c27343876f4fb3f8c51ecad
Amina <himri.amina@gmail.com>
2017-03-25 18:55:46 +0100

    User_auth modified

--- src/app/user-auth.service.ts
+++ src/app/user-auth.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Http} from "@angular/http";
 
 @Injectable()
 export class UserAuthService {
 
-  constructor() { }
+  constructor( private http : Http ) {}
+  login(username :string , password : string){
+    let body ={
+        app_name:  "ubsunu",
+        user_name: username,
+        password:  password,
+    };
+  return this.http.post("localhost:8080/token",body);
+  }
+
 
 }
