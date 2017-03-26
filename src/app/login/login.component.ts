1a1ad491491086e300d701fb88acaa1b43d83000
Amina <himri.amina@gmail.com>
2017-03-27 00:39:03 +0200

    user_auth modif

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {UserRoleService} from "../user-role.service";
+import {UserAuthService} from "../user-auth.service";
+import {Router} from "@angular/router";
+import {error} from "util";
 
 @Component({
   selector: 'app-login',
@@ -8,14 +11,28 @@ import {UserRoleService} from "../user-role.service";
 })
 export class LoginComponent implements OnInit {
 
-  constructor( public userRoleService: UserRoleService) { }
+  constructor(
+    public userRoleService: UserRoleService ,
+    private userAuthService: UserAuthService,
+    private router: Router
+  ) { }
+
 
   ngOnInit() {
   }
   username : string ;
   password : string;
   login(){
-    console.log(this.username);
-    console.log(this.password);
+    console.log (this.username);
+    console.log (this.password);
+    this.userAuthService
+      .login(this.username,this.password)
+      .subscribe(()=>{
+        this.router.navigate(['/welcome']);
+      },
+        (error)=>{
+          console.log(error)
+        });
   }
+
 }
--- src/app/user-auth.service.ts
+++ src/app/user-auth.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
-import {Http} from "@angular/http";
-
+import {Http, Response} from "@angular/http";
+import 'rxjs/Rx';
 @Injectable()
 export class UserAuthService {
 
   constructor( private http : Http ) {}
+    token : string ;
   login(username :string , password : string){
     let body ={
         app_name:  "ubsunu",
         user_name: username,
         password:  password,
     };
-  return this.http.post("localhost:8080/token",body);
+  return this.http.post("http://localhost:8080/token",body)
+    .map( (response : Response ) =>{
+    this.token= response.text();
+  }) ;
   }
 
 
