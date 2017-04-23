3b2cc88d2fb65a076372d3c791a65a416b8292ca
Amina <himri.amina@gmail.com>
2017-04-23 23:38:18 +0200

    profile settings

--- src/app/profile-settings/profile-settings.component.ts
+++ src/app/profile-settings/profile-settings.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileSettingsComponent implements OnInit {
 
+//   function_myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+//
+// // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
   constructor() { }
 
   ngOnInit() {
