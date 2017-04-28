04095fd4244733e69efae1ea9e20965b0b2e3da6
Amina <himri.amina@gmail.com>
2017-04-28 17:23:36 +0200

    comment

--- src/app/signup.service.ts
+++ src/app/signup.service.ts
@@ -3,30 +3,27 @@ import {Http} from "@angular/http";
 
 @Injectable()
 export class SignupService {
-  /*token : string ;
+
 
   constructor( private http : Http  ) {
-    this.token=null;
+
   }
-  signup(username :string , password : string){
-    let body ={
+  //signup(username :string , password : string){
+  //  let body ={
 
-        appName: "ubsunu",
-        userName: "himnabil",
-        password: "simNab31.",
-        userType: "teacher",
-        firstName: "Nabil",
-        lastName: "Himri",
-        birthDay: "04-07-1992",
-        email: "himnabil@gmail.com",
-        organisation: "Edelia"
+  //      appName: "ubsunu",
+  //      userName: "himnabil",
+  //      password: "simNab31.",
+  //      userType: "teacher",
+  //      firstName: "Nabil",
+  //      lastName: "Himri",
+  //      birthDay: "04-07-1992",
+  //      email: "himnabil@gmail.com",
+  //      organisation: "Edelia"
 
-    };
-    return this.http.post("api/alphau/token",body)
-      .map( (response : Response ) =>{
-        this.token= response.text();
-      })
+  //  };
 
-  }*/
+  //}
+
 
 }
